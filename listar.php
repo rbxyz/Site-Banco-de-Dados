407dbf733c0d2f7a7192210b98f92916b0f8d5af
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-10-02 09:22:41 -0300

    Update listar.php

--- listar.php
+++ listar.php
@@ -1,24 +1,23 @@
 
     <?php
-    include 'conexao.php'; 
+    include 'funcoes.php';
     $conexao = conectar();
-    function listarPessoas($conexao) {
-        echo "<table border='1'>";
-        echo "<h1>ID</h1><h2>Nome</h2><h2>Sobrenome</h2><h2>Rua</h2><h2>Número</h2><h2>Idade</h2></h2>";
-
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "<tr>";
-            echo "<td>".$row['id']."</td>";
-            echo "<td>".$row['nome']."</td>";
-            echo "<td>".$row['sobrenome']."</td>";
-            echo "<td>".$row['rua']."</td>";
-            echo "<td>".$row['numero']."</td>";
-            echo "<td>".$row['idade']."</td>";
-            echo "</tr>";
-        }
-
-        echo "</table>";
-    }
-    listarPessoas($conexao);
-    $conexao = null;
     ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Listar pessoas</title>
+    </head>
+    <body>
+        <a href="index.html">Voltar</a>
+        <br>
+        
+        <?php
+        listarPessoas($conexao);
+        $conexao = null;
+        ?>
+    </body>
+    </html>
