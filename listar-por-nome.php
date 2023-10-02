fb90bcd5d1f52ad685eef879b8ca1a32e7850715
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-10-02 09:21:56 -0300

    Update listar-por-nome.php

--- listar-por-nome.php
+++ listar-por-nome.php
@@ -1,8 +1,29 @@
-<? php
-function listarPorNomePesquisado($conexao, $aux) {
-    $sql = "SELECT * FROM pessoas WHERE nome LIKE :aux";
-    $stmt = $conexao->prepare($sql);
-    $stmt->bindValue(":aux", "%$aux%", PDO::PARAM_STR);
-    $stmt->execute();
-}
-?>
+    
+    <?php
+    include 'funcoes.php';
+    $conexao = conectar();
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Listar pessoas por nome</title>
+    </head>
+    <body>
+        <a href="index.html">Voltar</a>
+        <br>
+        <?php
+        $nome = $_POST['nome'];      
+        $retornodafuncaoinserida = listarPorNomePesquisado($conexao, $nome);
+        $conexao = null;
+
+        if ($retornodafuncaoinserida == true) {
+            echo("Listado os dados corretamente!");
+        }else{
+            echo("Não foi possivel listar os dados.");
+        }
+        ?>
+    </body>
+    </html>
