cc47cee77bd1aebc7a1eebcd1512f912daedff7d
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-10-02 09:20:29 -0300

    Update inserir.php

--- inserir.php
+++ inserir.php
@@ -1,31 +1,31 @@
-<?php
-include 'funcoes.php';
-$conexao = conectar();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inserir pessoas</title>
-</head>
-<body>
-    <a href="index.html">Voltar</a>
-    <br>
     <?php
-        $nome = $_POST['nome'];
-        $sobrenome = $_POST['sobrenome'];
-        $rua = $_POST['rua'];
-        $numero = $_POST['numero'];
-        $idade = $_POST['idade'];
-    $retornodafuncaoinserida = inserirPessoa($conexao, $nome, $sobrenome, $rua, $numero, $idade);
-    $conexao = null;
-    if ($retornodafuncaoinserida == true) {
-        echo("Inserido os dados corretamente!");
-    }else{
-        echo("Não foi possivel inserir os dados.");
-    }
-    
+    include 'funcoes.php';
+    $conexao = conectar();
     ?>
-</body>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Inserir pessoas</title>
+    </head>
+    <body>
+        <a href="index.html">Voltar</a>
+        <br>
+        <?php
+            $nome = $_POST['nome'];
+            $sobrenome = $_POST['sobrenome'];
+            $rua = $_POST['rua'];
+            $numero = $_POST['numero'];
+            $idade = $_POST['idade'];
+        $retornodafuncaoinserida = inserirPessoa($conexao, $nome, $sobrenome, $rua, $numero, $idade);
+        $conexao = null;
+        if ($retornodafuncaoinserida == true) {
+            echo("Inserido os dados corretamente!");
+        }else{
+            echo("Não foi possivel inserir os dados.");
+        }
+        
+        ?>
+    </body>
