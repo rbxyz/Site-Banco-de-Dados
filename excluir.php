f77b317d4d7d5b5412eaabd1c8daaee3354924ce
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-10-02 09:17:25 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,8 +1,30 @@
-<? php
-function excluirPessoa($conexao, $id) {
-    $sql = "DELETE FROM pessoas WHERE id = :id";
-    $stmt = $conexao->prepare($sql);
-    $stmt->bindParam(":id", $id, PDO::PARAM_INT);
-    return $stmt -> execute();
-}
-?>
+    
+    <?php
+    include 'funcoes.php';
+    $conexao = conectar();
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Excluir pessoas</title>
+    </head>
+    <body>
+        <a href="index.html">Voltar</a>
+        <br>
+        <?php
+        $id = $_POST['id'];
+        listarPessoas($conexao);
+        
+        $retornodafuncaoinserida = excluirPessoa($conexao, $id);
+        $conexao = null;
+        if ($retornodafuncaoinserida == true) {
+            echo("Excluído os dados corretamente!");
+        }else{
+            echo("Não foi possivel excluir os dados.");
+        }
+        ?>
+    </body>
+    </html>
