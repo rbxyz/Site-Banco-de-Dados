43a94884890adfdd2166566e6946e6126cbc4bcb
Ruan Gabriel <110505289+rbxyz@users.noreply.github.com>
2023-10-02 09:19:20 -0300

    Update inserir.php

--- inserir.php
+++ inserir.php
@@ -1,12 +1,31 @@
-<? php
-function inserirPessoa($conexao, $nome, $sobrenome, $rua, $numero, $idade) {
-    $sql = "INSERT INTO pessoas (nome, sobrenome, rua, numero, idade) VALUES (:nome, :sobrenome, :rua, :numero, :idade)";
-    $stmt = $conexao->prepare($sql);
-    $stmt->bindParam(":nome", $nome, PDO::PARAM_STR);
-    $stmt->bindParam(":sobrenome", $sobrenome, PDO::PARAM_STR);
-    $stmt->bindParam(":rua", $rua, PDO::PARAM_STR);
-    $stmt->bindParam(":numero", $numero, PDO::PARAM_INT);
-    $stmt->bindParam(":idade", $idade, PDO::PARAM_INT);
-    return $stmt->execute();
-}
+<?php
+include 'funcoes.php';
+$conexao = conectar();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inserir pessoas</title>
+</head>
+<body>
+    <a href="index.html">Voltar</a>
+    <br>
+    <?php
+        $nome = $_POST['nome'];
+        $sobrenome = $_POST['sobrenome'];
+        $rua = $_POST['rua'];
+        $numero = $_POST['numero'];
+        $idade = $_POST['idade'];
+    $retornodafuncaoinserida = inserirPessoa($conexao, $nome, $sobrenome, $rua, $numero, $idade);
+    $conexao = null;
+    if ($retornodafuncaoinserida == true) {
+        echo("Inserido os dados corretamente!");
+    }else{
+        echo("Não foi possivel inserir os dados.");
+    }
+    
+    ?>
+</body>
